4bbe7bde647b7390a373fac4f99180c5b8f4dcd1
luweijun <214245088@qq.com>
2023-07-17 11:18:50 +0800

    1

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post,Req,Request,Query,HttpCode,Header,Redirect ,Bind,Param,Body} from '@nestjs/common';
 import { UserService } from './user.service';
 import { ConfigService } from '@nestjs/config';
 import { ConfigEnum } from '../enum/config.enum';
+
+interface CreateUserDto{
+  name: string;
+  age: number;
+}
+
 @Controller('user')
 export class UserController {
   constructor(
@@ -25,9 +31,72 @@ export class UserController {
     return this.appService.getUser(obj);
   }
 
+  @Get('test')
+  getHelloReq(@Query() request): string {
+    console.log(request)
+    return 'asdsadasd';
+  }
+  // @Get('*')
+  // getHelloReq1(): string {
+  //   return 'asdsadasd11';
+  // }
+
+  /** 状态码 */
+  // 返回状态码 204
+  @Post()
+  @HttpCode(201)
+  @Header('Cache-Control', 'none')
+  create_status_code() {
+    return {
+      name: 'lwj'
+    }
+  }
+
+  @Get('redirect')
+  @Redirect('https://www.baidu.com', 301)
+  // 任意方法名 
+  redirect(){}
+
+  /*
+   * 访问 http://localhost:3000/cats/docs?version=5 跳转到 https://docs.nestjs.com/v5/'
+   * 访问 http://localhost:3000/cats/docs 跳转到 https://docs.nestjs.com'
+   */
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
+  /** 路由参数 */
+  @Get('param1/:id')
+  findOne1(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a param1 #${params.id} cat`;
+  }
+
+  @Get('param2/:id')
+  @Bind(Param())
+  findOne2(params) {
+    console.log(params.id);
+    return `This action returns a param2 #${params.id} cat`;
+  }
+
+  @Get('param3/:id')
+  findOne3(@Param('id') id: string): string {
+    return `This action returns a param3 #${id} cat`;
+  }
+
+  @Get('param4/:id')
+  @Bind(Param('id'))
+  findOne4(id) {
+    return `This action returns a param4 #${id} cat`;
+  }
+
   @Post('add')
-  addHello(): any {
-    console.log(111);
+  addHello(@Body() createUserDto: CreateUserDto): any {
+    console.log(111,createUserDto.name,createUserDto.age);
     return this.appService.addUser();
   }
 }
